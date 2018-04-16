2975c44d6d8f3b18e6f50095f2bd7c440bea22df
India Amos <india@indiamos.com>
2018-04-15 20:18:21 -0400

    Move component into new file, add prettier

---
+++ js/ClientApp.jsx
@@ -0,0 +1,14 @@
+const ce = React.createElement;
+const MyTitle = function(props) {
+  return ce('div', null, ce('h1', { style: { color: props.color } }, props.title));
+};
+const MyFirstComponent = function() {
+  return ce(
+    'div',
+    { id: 'my-first-component' },
+    ce(MyTitle, { title: 'Game of Thrones', color: 'YellowGreen' }),
+    ce(MyTitle, { title: 'Stranger Things', color: 'GreenYellow' }),
+    ce(MyTitle, { title: 'Rick and Morty', color: 'LimeGreen' }),
+  );
+};
+ReactDOM.render(ce(MyFirstComponent), document.getElementById('app'));
